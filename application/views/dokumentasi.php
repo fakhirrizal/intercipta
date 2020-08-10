e0dce4ddc735ef5960928e77f9d0d0394818b6fe
fakhi_rizal <bokir.rizal@gmail.com>
2020-08-10 13:52:36 +0700

    update

--- application/views/dokumentasi.php
+++ application/views/dokumentasi.php
@@ -1,24 +1,5 @@
 <!DOCTYPE html>
-<!-- 
-Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.6
-Version: 4.5.6
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!-->
 <html lang="en">
-    <!--<![endif]-->
-    <!-- BEGIN HEAD -->
-
     <head>
         <meta charset="utf-8" />
         <title>Apps</title>
@@ -28,22 +9,21 @@ License: You must have a valid license purchased only from themeforest(the above
         <meta content="" name="author" />
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
         <!-- END GLOBAL MANDATORY STYLES -->
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="http://datum.aplikasiku.online/assets/global/css/components-md.min.css" rel="stylesheet" id="style_components" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/global/css/plugins-md.min.css" rel="stylesheet" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/pages/css/blog.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/global/css/components-md.min.css" rel="stylesheet" id="style_components" type="text/css" />
+        <link href="<?= base_url(); ?>assets/global/css/plugins-md.min.css" rel="stylesheet" type="text/css" />
         <!-- END THEME GLOBAL STYLES -->
         <!-- BEGIN THEME LAYOUT STYLES -->
-        <link href="http://datum.aplikasiku.online/assets/layouts/layout3/css/layout.min.css" rel="stylesheet" type="text/css" />
-        <link href="http://datum.aplikasiku.online/assets/layouts/layout3/css/themes/default.min.css" rel="stylesheet" type="text/css" id="style_color" />
-        <link href="http://datum.aplikasiku.online/assets/layouts/layout3/css/custom.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/layouts/layout3/css/layout.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?= base_url(); ?>assets/layouts/layout3/css/themes/default.min.css" rel="stylesheet" type="text/css" id="style_color" />
+        <link href="<?= base_url(); ?>assets/layouts/layout3/css/custom.min.css" rel="stylesheet" type="text/css" />
         <!-- END THEME LAYOUT STYLES -->
-        <link rel="shortcut icon" href="http://datum.aplikasiku.online/assets/icon.png" /> </head>
+        <link href="<?=base_url('assets/icon.png');?>" rel="icon" type="image/x-icon"> </head>
     <!-- END HEAD -->
 
         <body class="page-container-bg-solid page-md">
@@ -3791,22 +3771,18 @@ License: You must have a valid license purchased only from themeforest(the above
 		<img src='http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-5/256/navigate-up-icon.png' width='20%' />
         </div>
         <!-- END FOOTER -->
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+        <script src="<?= base_url(); ?>assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+        <script src="<?= base_url(); ?>assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+      
+        <script src="<?= base_url(); ?>assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+        <script src="<?= base_url(); ?>assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+        <script src="<?= base_url(); ?>assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
         <!-- END CORE PLUGINS -->
         <!-- BEGIN THEME GLOBAL SCRIPTS -->
-		<script src="http://datum.aplikasiku.online/assets/global/scripts/app.min.js" type="text/javascript"></script>
-		<!-- END THEME GLOBAL SCRIPTS -->
+        <script src="<?= base_url(); ?>assets/global/scripts/app.min.js" type="text/javascript"></script>
+        <!-- END THEME GLOBAL SCRIPTS -->
         <!-- BEGIN THEME LAYOUT SCRIPTS -->
-        <script src="http://datum.aplikasiku.online/assets/layouts/layout3/scripts/layout.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/layouts/layout3/scripts/demo.min.js" type="text/javascript"></script>
-        <script src="http://datum.aplikasiku.online/assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
-        <!-- END THEME LAYOUT SCRIPTS -->
+        <script src="<?= base_url(); ?>assets/layouts/layout3/scripts/layout.min.js" type="text/javascript"></script>
     </body>
 
 </html>
